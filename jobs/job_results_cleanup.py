1826598db0833f8770b752471cd96d556e76e036
Jeff Kala <48843785+jeffkala@users.noreply.github.com>
2023-04-25 11:16:37 -0600

    Update job_results_cleanup.py

--- jobs/job_results_cleanup.py
+++ jobs/job_results_cleanup.py
@@ -25,15 +25,17 @@ class ClearJobResultsJob(Job):
     def run(self, data, commit):
         """Run a job to remove legacy reservations."""
         end_time = data.get("days_to_keep")
-
-        for job_result in JobResult.objects.filter(
+        
+        jobs_to_delete = JobResult.objects.filter(
             completed__lte=str(make_aware(datetime.datetime.now()) - datetime.timedelta(days=end_time))
-        ):
+        )
+        for job_result in jobs_to_delete:
             if not commit:
                 self.log_info(obj=job_result, message=f"{job_result} would be removed if Commit changes was selected.")
-            if commit:
-                job_result.delete()
-        self.log_success(obj=None, message=f"Job results older than {end_time} days old have been deleted.")
+                self.log_success(obj=None, message="Dry Run successfully completed.")
+        if commit:
+            jobs_to_delete.delete()
+            self.log_success(obj=None, message=f"Job results older than {end_time} days old have been deleted.")
 
 
 jobs = [ClearJobResultsJob]

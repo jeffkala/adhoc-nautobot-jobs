ecbfeba85247b242b5c5e4064367a423a7182bee
Jeff Kala <48843785+jeffkala@users.noreply.github.com>
2024-02-07 15:40:05 -0700

    Update __init__.py

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,5 +1,5 @@
 from nautobot.apps.jobs import register_jobs
-from tcp_connection_check import ConnectivityCheckTask
+from .tcp_connection_check import ConnectivityCheckTask
 
 jobs = [ConnectivityCheckTask]
 register_jobs(*jobs)

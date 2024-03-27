39eb2957eab990acbbfd4872fefbbe1676619ed6
Jeff Kala <jeff.l.kala@gmail.com>
2024-03-27 15:12:51 -0500

    nbc secret rotation

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -2,7 +2,7 @@
 
 from nautobot.apps.jobs import register_jobs
 from .tcp_connection_check import ConnectivityCheckTask
-from .nbc_secret_rotation import CloudSecretRotation
+from .nbc_secret_rotation import CloudSecretRotationv1, CloudSecretRotationv2
 
-jobs = [ConnectivityCheckTask, CloudSecretRotation]
+jobs = [ConnectivityCheckTask, CloudSecretRotationv1, CloudSecretRotationv2]
 register_jobs(*jobs)

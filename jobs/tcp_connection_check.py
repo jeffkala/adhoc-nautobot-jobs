92a11193f4f7f647653bb654989061eb55e63a13
Jeff Kala <48843785+jeffkala@users.noreply.github.com>
2025-03-31 16:16:06 -0600

    Update tcp_connection_check.py

--- jobs/tcp_connection_check.py
+++ jobs/tcp_connection_check.py
@@ -59,7 +59,7 @@ class ConnectivityCheckTask(Job):  # pylint: disable=too-many-instance-attribute
         # g = group(sig,)
         # res = g(routing_key="default", hostname=gethostname())
         # sig = signature('nautobot.extras.jobs.run_job', args=('jobs.jobs.tcp_connection_check.ConnectivityCheckTask',{}), celery_kwargs=self.job_result.celery_kwargs, routing_key="default", kwargs=tkwargs)
-        sig = signature('nautobot_plugin_nornir.jobs.hello_world', args('hello_world',), kwargs={}, routing_key="default", hostname=gethostname())
+        sig = signature('nautobot_plugin_nornir.jobs.hello_world', args=('hello_world',), kwargs={}, routing_key="default", hostname=gethostname())
         g = group(sig,)
         res = g()
         self.logger.info("res: %s", res.results)

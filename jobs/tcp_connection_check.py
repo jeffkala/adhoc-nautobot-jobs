ba6511148dcf2fd75f4a9fce2ffef9a6b8825f04
Jeff Kala <48843785+jeffkala@users.noreply.github.com>
2025-03-31 16:00:15 -0600

    Update tcp_connection_check.py

--- jobs/tcp_connection_check.py
+++ jobs/tcp_connection_check.py
@@ -36,11 +36,6 @@ class ConnectivityCheckTask(Job):  # pylint: disable=too-many-instance-attribute
             inventory={
                 "plugin": "nautobot-inventory",
                 "options": {
-                    "runner": {
-                    "plugin": "threaded",
-                    "options": {
-                        "num_workers": 20,
-                    },
                     "credentials_class": "nautobot_plugin_nornir.plugins.credentials.env_vars.CredentialsEnvVars",
                     "queryset": Device.objects.first(),
                     "defaults": {"now": datetime.now()},

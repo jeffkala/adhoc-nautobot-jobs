aae0462873ab452f04879ce866e4cd49fe550ab1
Jeff Kala <48843785+jeffkala@users.noreply.github.com>
2025-03-31 15:56:47 -0600

    Update tcp_connection_check.py

--- jobs/tcp_connection_check.py
+++ jobs/tcp_connection_check.py
@@ -3,6 +3,8 @@ from netutils.ping import tcp_ping
 from celery import group
 from celery import signature
 from celery.utils.nodenames import gethostname
+from nautobot.dcim.models import Device
+
 
 class ConnectivityCheckTask(Job):  # pylint: disable=too-many-instance-attributes
     """Nautobot Job for checking a tcp port is 'opened'."""
@@ -19,6 +21,7 @@ class ConnectivityCheckTask(Job):  # pylint: disable=too-many-instance-attribute
         name = "TCP Connectivity Check"
         description = "Run Netutils TCP_PING function and report status."
 
+   
     def run(self, *args, **kwargs):
         """Process tcp_ping task from job."""
         self.logger.info(f"{dir(self)}")
@@ -27,11 +30,26 @@ class ConnectivityCheckTask(Job):  # pylint: disable=too-many-instance-attribute
         self.logger.info(f"{self.job_result.celery_kwargs}")
         # args: ['jobs.jobs.tcp_connection_check.ConnectivityCheckTask']
         tkwargs = {'ip_addresses': '10.1.1.9', 'port': 22}
+        nr = InitNornir(
+            runner=NORNIR_SETTINGS.get("runner"),
+            logging={"enabled": False},
+            inventory={
+                "plugin": "nautobot-inventory",
+                "options": {
+                    "credentials_class": NORNIR_SETTINGS.get("credentials"),
+                    "params": NORNIR_SETTINGS.get("inventory_params"),
+                    "queryset": Device.objects.first(),
+                    "defaults": {"now": datetime.now()},
+                },
+            },
+        )
+        result = nr.run(task=hello_world)
+        print(result)
         # sig = signature('nautobot.extras.jobs.run_job', args=('jobs.jobs.tcp_connection_check.ConnectivityCheckTask',{'properties':{'exchange': '', 'routing_key': 'default'}}), routing_key="default", kwargs=tkwargs)
         # g = group(sig,)
         # res = g(routing_key="default", hostname=gethostname())
         # sig = signature('nautobot.extras.jobs.run_job', args=('jobs.jobs.tcp_connection_check.ConnectivityCheckTask',{}), celery_kwargs=self.job_result.celery_kwargs, routing_key="default", kwargs=tkwargs)
-        sig = signature('nautobot_plugin_nornir.jobs.hello_world', kwargs={}, routing_key="default", hostname=gethostname())
+        sig = signature('nautobot_plugin_nornir.jobs.hello_world', kwargs={'tasks': 'hellow_world'}, routing_key="default", hostname=gethostname())
         g = group(sig,)
         res = g()
         self.logger.info("res: %s", res.results)

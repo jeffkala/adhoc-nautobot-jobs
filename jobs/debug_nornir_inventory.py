2b461c391f9653771d9b0fbd93f2b8166028fc18
Jeff Kala <48843785+jeffkala@users.noreply.github.com>
2025-04-03 20:54:42 -0600

    Update debug_nornir_inventory.py

--- jobs/debug_nornir_inventory.py
+++ jobs/debug_nornir_inventory.py
@@ -1,65 +1,77 @@
-# """Jobs for nautobot-plugin-nornir debugging."""
-# from datetime import datetime
+"""Jobs for nautobot-plugin-nornir debugging."""
 
-# from nautobot.core.celery import register_jobs
-# from nautobot.dcim.models import Device
-# from nautobot.extras.jobs import Job
-# from nornir import InitNornir
-# from nornir.core.plugins.inventory import InventoryPluginRegister
-# from nautobot_plugin_nornir.constants import NORNIR_SETTINGS
-# from nautobot_plugin_nornir.plugins.inventory.nautobot_orm import NautobotORMInventory
+from datetime import datetime
 
-# from utilities.helpers import FormEntry, get_job_filter
+import yaml
+from nautobot.apps.jobs import Job, MultiObjectVar, register_jobs
+from nautobot.dcim.models import Device
+from nornir import InitNornir
+from nornir.core.plugins.inventory import InventoryPluginRegister
 
-# InventoryPluginRegister.register("nautobot-inventory", NautobotORMInventory)
+from nautobot_plugin_nornir.constants import NORNIR_SETTINGS
+from nautobot_plugin_nornir.plugins.inventory.nautobot_orm import NautobotORMInventory
 
-
-# class DebugInventoryJob(Job, FormEntry):
-#     """Job to debug stuff."""
-
-#     class Meta:  # pylint: disable=too-few-public-methods
-#         """Meta object boilerplate for compliance."""
-
-#         name = "Debug Nornir Inventory"
-#         description = "Prints inventory details per host, group, defaults."
-#         has_sensitive_variables = False
-#         hidden = True
-
-#     def run(self, *args, **data):  # pylint: disable=too-many-branches
-#         """Run config compliance report script."""
-#         # pylint: disable=unused-argument
-#         qs = get_job_filter(data)
-#         try:
-#             with InitNornir(
-#                 runner=NORNIR_SETTINGS.get("runner"),
-#                 logging={"enabled": False},
-#                 inventory={
-#                     "plugin": "nautobot-inventory",
-#                     "options": {
-#                         "credentials_class": NORNIR_SETTINGS.get("credentials"),
-#                         "params": NORNIR_SETTINGS.get("inventory_params"),
-#                         "queryset": qs,
-#                         "defaults": {"now": datetime.now()},
-#                     },
-#                 },
-#             ) as nornir_obj:
-#                 for host, data in nornir_obj.inventory.hosts.items():
-#                     self.logger.info(
-#                         "#### %s Data\n%s",
-#                         host,
-#                         "\n".join(f"{k}: {v}\n" for k, v in data.dict().items()),
-#                         extra={"object": Device.objects.get(id=data.data["id"])},
-#                     )
-#                 self.logger.info(
-#                     "#### Default Data\n%s",
-#                     nornir_obj.inventory.defaults.data,
-#                 )
-#                 self.logger.info("Total In Scope Devices: %s", len(nornir_obj.inventory.hosts.items()))
-#         except Exception as err:
-#             self.logger.info("%s", err)
-#             raise
+InventoryPluginRegister.register("nautobot-inventory", NautobotORMInventory)
 
 
-# jobs = [DebugInventoryJob]
+class DebugInventoryJob(Job):
+    """Job to debug Nornir Inventory."""
 
-# register_jobs(*jobs)
+    device = MultiObjectVar(model=Device, required=False)
+
+    class Meta:  # pylint: disable=too-few-public-methods
+        """Meta object boilerplate for compliance."""
+
+        name = "Debug Nornir Inventory"
+        description = "Prints inventory details per host, group, defaults."
+        has_sensitive_variables = True
+
+    def run(self, *args, **data):  # pylint: disable=too-many-branches
+        """Run config compliance report script."""
+        now = datetime.now()
+        qs = Device.objects.filter(id__in=[dev.id for dev in data["device"]])
+        after_qs = datetime.now()
+        self.logger.info("Queryset Took\n%s", after_qs - now)
+        self.logger.info("Queryset Count\n%s", qs.count())
+        try:
+            now = datetime.now()
+            with InitNornir(
+                runner=NORNIR_SETTINGS.get("runner"),
+                logging={"enabled": False},
+                inventory={
+                    "plugin": "nautobot-inventory",
+                    "options": {
+                        "credentials_class": NORNIR_SETTINGS.get("credentials"),
+                        "params": NORNIR_SETTINGS.get("inventory_params"),
+                        "queryset": qs,
+                        "defaults": {"now": datetime.now()},
+                    },
+                },
+            ) as nornir_obj:
+                after_qs = datetime.now()
+                self.logger.info("Nonir Init Took\n%s", after_qs - now)
+                for host, data in nornir_obj.inventory.hosts.items():
+                    self.logger.info(
+                        "#### %s\n```yaml\n%s```",
+                        host,
+                        yaml.dump(nornir_obj.inventory.hosts[host], default_flow_style=False),
+                        extra={"object": Device.objects.get(id=data.data["id"])},
+                    )
+                    self.logger.info(
+                        "#### %s Data\n```yaml\n%s```",
+                        host,
+                        yaml.dump(data.dict(), default_flow_style=False),
+                        extra={"object": Device.objects.get(id=data.data["id"])},
+                    )
+                self.logger.info(
+                    "#### Default Data\n```yaml\n%s```",
+                    yaml.dump(nornir_obj.inventory.defaults.data, default_flow_style=False),
+                )
+        except Exception as err:
+            self.logger.info("%s", err)
+            raise
+
+
+jobs = [DebugInventoryJob]
+
+register_jobs(*jobs)
